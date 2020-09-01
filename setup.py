0abf5c3b9c8660ca551cf86491cf4914bc9e9251
Shrey Shah <41659522+ZeroPanda@users.noreply.github.com>
2020-08-31 23:28:53 -0400

    installation file

---
+++ setup.py
@@ -0,0 +1,26 @@
+# ----- Imports ---------------------------------------------------------------
+
+from distutils.core import setup
+
+# ----- Instructions ----------------------------------------------------------
+
+setup(
+    name = 'coinbasepro',
+    version = '0.0.1',
+    packages = ['coinbasepro'],
+    url = 'https://github.com/ZeroPanda/CoinBase-Pro-API',
+    license = 'MIT',
+    author = 'Shrey Shah',
+    author_email = 'shreyshah31@gmail.com',
+    description = 'Python Coinbase Pro API',
+    classifiers = [
+        'Development Status :: Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
