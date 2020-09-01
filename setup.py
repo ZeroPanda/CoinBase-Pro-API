af96635578e7df57e30ff0962c5eddf5cd6c9950
Shrey Shah <41659522+ZeroPanda@users.noreply.github.com>
2020-08-31 23:33:01 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,16 @@ from distutils.core import setup
 
 # ----- Instructions ----------------------------------------------------------
 
+REQUIREMENTS = [line.strip() for line in open(os.path.join(os.path.dirname(__file__),'requirements.txt')).readlines()]
+
+
 setup(
     name = 'coinbasepro',
     version = '0.0.1',
     packages = ['coinbasepro'],
     url = 'https://github.com/ZeroPanda/CoinBase-Pro-API',
     license = 'MIT',
+    install_requires=REQUIREMENTS,
     author = 'Shrey Shah',
     author_email = 'shreyshah31@gmail.com',
     description = 'Python Coinbase Pro API',
