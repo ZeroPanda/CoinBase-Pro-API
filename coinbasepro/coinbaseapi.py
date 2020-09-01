77e36b95e83b9b0a330b8cafdfd415f6c35d2d5d
Shrey Shah <41659522+ZeroPanda@users.noreply.github.com>
2020-09-01 01:59:35 -0400

    Update coinbaseapi.py

--- coinbasepro/coinbaseapi.py
+++ coinbasepro/coinbaseapi.py
@@ -9,6 +9,8 @@ class coinpy(object):
         self.auth = CoinbaseExchangeAuth(api_key, api_secret, passcode)
 
     
+    ### All the GET requuests ###
+
     def get_accounts(self,account_id=''):
         # Get account information
         r = requests.get(self.api_url + 'accounts/' + account_id, auth=self.auth)
@@ -29,8 +31,124 @@ class coinpy(object):
         print(r.json())
         return r.json()
 
+    def list_orders(self, order_id=''):
+        # Get order information in detail
+        r = requests.get(self.api_url + 'orders/' +
+                         order_id, auth=self.auth)
+        print(r.json())
+        return r.json()
+
     def list_fills(self):
         # Get a list of recent fills of the API key's profile.
         r = requests.get(self.api_url + 'fills', auth=self.auth)
         print(r.json())
         return r.json()
+
+    def exchange_limits(self):
+        # Get Current Exchange Limits
+        r = requests.get(
+            self.api_url + '/users/self/exchange-limits', auth=self.auth)
+        print(r.json())
+        return r.json()
+
+    def list_deposits(self,transfer_id=''):
+        # Get trnasfers
+        try:
+            r = requests.get(self.api_url + '/transfers' + transfer_id, auth=self.auth)
+        except:
+            r = requests.get(self.api_url + '/transfers/:' +
+                             transfer_id, auth=self.auth)
+        print(r.json())
+        return r.json()
+
+    def payment_method(self):
+        # Get the payment method
+        r = requests.get(
+            self.api_url + '/payment-methods', auth=self.auth)
+        print(r.json())
+        return r.json()
+
+    def coinbase_accounts(self):
+        # Get the coinbase account
+        r = requests.get(
+            self.api_url + '/coinbase-accounts', auth=self.auth)
+        print(r.json())
+        return r.json()
+
+    def current_fees(self):
+        # Get the current fees
+        r = requests.get(
+            self.api_url + '/fees', auth=self.auth)
+        print(r.json())
+        return r.json()
+
+    def get_profiles(self,profile_id=''):
+        # List your profiles
+        r = requests.get(
+            self.api_url + '/profiles/' + profile_id, auth=self.auth)
+        print(r.json())
+        return r.json()
+
+    def trailing_volume(self):
+        # Get the trailing volume
+        r = requests.get(
+            self.api_url + '/users/self/trailing-volume', auth=self.auth)
+        print(r.json())
+        return r.json()
+
+    def margin(self,para):
+        # Get margin profile_information/buying_power/withdrawal_power/withdrawal_power_all/exit_plan/liquidation_history/position_refresh_amounts/status
+        try:
+            r = requests.get(
+                self.api_url + '/margin/' + para, auth=self.auth)
+            print(r.json())
+            return r.json()
+        except NameError:
+            pass
+        
+    def oracle(self):
+        # get the open oracle values
+        r = requests.get(
+            self.api_url + '/oracle', auth=self.auth)
+        print(r.json())
+        return r.json()
+
+    ## Get the market data ##
+
+    def get_products(self):
+        # Get all the product information
+        r = requests.get(self.api_url + '/products' , auth=self.auth)
+        print(r.json())
+        return r.json()
+
+    def get_product_info(self, product_id,para=''):
+        # Get product information , book/ticker/trades/candles/stats
+        r = requests.get(self.api_url + '/products/' + product_id +'/' + para,auth=self.auth)
+        print(r.json())
+        return r.json()
+
+    def get_currencies(self):
+        # Get currency information
+        r = requests.get(self.api_url + '/currencies', auth=self.auth)
+        print(r.json())
+        return r.json()
+
+    def get_time(self):
+        # Get the API server time
+        r = requests.get(self.api_url + '/time', auth=self.auth)
+        print(r.json())
+        return r.json()
+
+    ### POST ###
+    # orders
+    # deposit/payment-method
+    # deposits/coinbase-account
+    # withdrawals/payment-method
+    # withdrawals/coinbase-account
+    # withdrawals/crypto
+    # conversions
+    # reports
+    # profiles/transfer
+
+    ### DELETE ###
+    # orders
